09dca3e9a78bdba20bab441f6792a929dfb2753e
Vinicius Godinho Del Rio <vinicius.delrio41@gmail.com>
2023-09-30 09:49:51 -0300

    feat: player simple movement

---
+++ Player/Player.gd
@@ -0,0 +1,11 @@
+extends CharacterBody2D
+
+func _physics_process(delta):
+	var input_vector = Vector2.ZERO
+	
+	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+
+	velocity = input_vector
+	
+	move_and_collide(velocity)
