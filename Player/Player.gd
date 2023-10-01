c95a29471394c72e7100b0a79f403890e4bff6d9
Vinicius Godinho Del Rio <vinicius.delrio41@gmail.com>
2023-10-01 15:09:19 -0300

    feat: player smooth movement

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,11 +1,20 @@
 extends CharacterBody2D
 
+const ACCELERATION = 10
+const MAX_SPEED = 100
+const FRICTION = 10
+
 func _physics_process(delta):
 	var input_vector = Vector2.ZERO
 	
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	input_vector = input_vector.normalized()
 
-	velocity = input_vector
+	if input_vector != Vector2.ZERO:
+		velocity += input_vector * ACCELERATION * delta
+		velocity = velocity.limit_length(MAX_SPEED * delta)
+	else:
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 	
 	move_and_collide(velocity)
