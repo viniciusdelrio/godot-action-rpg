aaff83c0d52d19a0dc9d64bb3a81414a5f3b4687
Vinicius Godinho Del Rio <vinicius.delrio41@gmail.com>
2023-10-15 19:35:47 -0300

    feat: add player idle and run animation working properly

--- Player/Player.gd
+++ Player/Player.gd
@@ -4,6 +4,10 @@ const ACCELERATION = 500
 const MAX_SPEED = 80
 const FRICTION = 500
 
+@onready var animationPlayer = $AnimationPlayer
+@onready var animationTree = $AnimationTree
+@onready var animationState = animationTree.get("parameters/playback")
+
 func _physics_process(delta):
 	var input_vector = Vector2.ZERO
 	
@@ -12,8 +16,12 @@ func _physics_process(delta):
 	input_vector = input_vector.normalized()
 
 	if input_vector != Vector2.ZERO:
+		animationTree.set("parameters/Idle/blend_position", input_vector)
+		animationTree.set("parameters/Run/blend_position", input_vector)
+		animationState.travel("Run")
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 	else:
+		animationState.travel("Idle")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 	
 	move_and_slide()
